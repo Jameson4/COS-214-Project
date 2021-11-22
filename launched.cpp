df3db725785f7f29b3921eda92312cda004b4c43
DurandtUys <81296952+DurandtUys@users.noreply.github.com>
2021-11-22 11:04:59 +0200

    Update launched.cpp

--- launched.cpp
+++ launched.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 void launched::changeState(string s)
-{
-    rocketState::changeState(s);
+{   
+    if(rocketState::getState() == "Rocket is ready to be launched.")
+    {
+        rocketState::changeState(s);
+    }
+    else
+    {
+        cout << "Rocket is not ready to be launched or has already been launched!" << endl;
+    }
 }
 
 string launched::getState()
