6a2d7d67ec76ea1e72dca434907b1cec4bb61782
DurandtUys <81296952+DurandtUys@users.noreply.github.com>
2021-11-22 11:39:32 +0200

    Create landed.h

---
+++ landed.h
@@ -0,0 +1,16 @@
+#ifndef LANDED_H
+#define LANDED_H
+
+#include "rocektState.h"
+#include <iostream>
+
+using namespace std;
+
+class landed
+{
+    public:
+        void changeState(string);
+        string getState()
+};
+
+#endif
